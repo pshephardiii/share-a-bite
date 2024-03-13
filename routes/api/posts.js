a116817b98f0d9b18ab20172e78ea3f68104a14d
Nicole Beechler <nmbeechler@gmail.com>
2024-03-13 19:44:49 -0400

    Minor update to routes/post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,14 +4,14 @@ const postCtrl = require('../../controllers/api/posts')
 const userCtrl = require('../../controllers/api/users')
 
 // Index GET /posts
-router.get('/', postCtrl.index, postCtrl.jsonBlogs)
+router.get('/', postCtrl.index, postCtrl.jsonPosts)
 // Delete DELETE /posts/:id
-router.delete('/:id', userCtrl.auth, postCtrl.destroy, postCtrl.jsonBlog)
+router.delete('/:id', userCtrl.auth, postCtrl.destroy, postCtrl.jsonPost)
 // Update PUT /posts/:id
-router.put('/:id', userCtrl.auth, blogCtrl.update, postCtrl.jsonBlog)
+router.put('/:id', userCtrl.auth, blogCtrl.update, postCtrl.jsonPost)
 // Create GET /posts
-router.post('/', userCtrl.auth, postCtrl.create, postCtrl.jsonBlog)
+router.post('/', userCtrl.auth, postCtrl.create, postCtrl.jsonPost)
 // Show GET /posts/:id
-router.get('/:id', postCtrl.show, postCtrl.jsonBlog)
+router.get('/:id', postCtrl.show, postCtrl.jsonPost)
 
 module.exports = router
