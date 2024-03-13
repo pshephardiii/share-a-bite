9c20dc54cc757d2b4c443d5b47614f2974cbea62
Nicole Beechler <nmbeechler@gmail.com>
2024-03-12 20:11:45 -0400

    Post model

--- models/post.js
+++ models/post.js
@@ -1,17 +1,15 @@
 const { model, Schema } = require('mongoose')
 
-
 const postSchema = new Schema ({
     title: { type: String, required: true },
     body: { type: String, required: false }, 
     pic: { type: String, required: false }, // picture of dish
     dish: { type: String, required: false }, // dish name
     rating: { type: Boolean, required: false }, 
+    user: { type: Schema.Types.ObjectId, ref: 'User'}, 
     restaurant: { type: Schema.Types.ObjectId, ref: 'Restaurants' }, 
     comments: { type: Schema.Types.ObjectId, ref: 'Comments' }, 
-    likedBy: { type: Schema.Types.ObjectId, ref: 'User' }, 
-    user: { type: Schema.Types.ObjectId, ref: 'User'}, 
-
+    likedBy: { type: Schema.Types.ObjectId, ref: 'User' }
 }, {
     timestamps: true
 })
