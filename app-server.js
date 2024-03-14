e9a9ccddd71d734b9435e8b8869aa8dad34b8938
Paul Shephard <paulshephard598@gmail.com>
2024-03-13 20:16:29 -0400

    changed restaurant routes file name

--- app-server.js
+++ app-server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'public', 'img','logo.png')))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/api/todos', require('./routes/api/todos'))
+app.use('/api/restaurants', require('./routes/api/restaurants'))
 // http://localhost:8000/api/todos/completed
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
