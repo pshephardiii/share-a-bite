debc8459d4c090d707cb1f67c204e45eae71d734
Nicole Beechler <nmbeechler@gmail.com>
2024-03-16 11:19:18 -0400

    Added login, sign up functions.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import LoginForm from "../../components/LoginForm/LoginForm"
+import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import { Link, useNavigate } from 'react-router-dom'
 import { Navigation } from '../../components/NavBar'
 import Restaurant from '../../components/Restaurant/Restaurant'
 import RestaurantList from '../../components/RestaurantList/RestaurantList'
 import RestaurantSampler from '../../components/RestaurantSampler/RestaurantSampler'
 
-export default function LandingPage(
-    { user, setUser }
-){
+export default function LandingPage(props){
+    const [showLogin, setShowLogin] = useState(true)
+}{
     const [restaurant, setRestaurant] = useState('')
     const [restaurants, setRestaurants] = useState([])
     const [restaurantSampler, setRestaurantSampler] = useState([])
@@ -15,6 +17,35 @@ export default function LandingPage(
     const navigate = useNavigate()
 
     return{
-       
+        <div className={styles.authButton}>
+        <button onClick={() => setShowLogin(!showLogin)}>{!showLogin? 'Already Have An account. Click Here To Sign In': 'New Here. Click Here Sign Up'}</button>
+        { !showLogin ? <SignUpForm signUp={props.signUp}/> : <LoginForm login={props.login}/>}    
+        </div>
     }
+}
+
+export default function LandingPage(props) {
+    // { user, setUser } /* Does this go here, or on the home page? */
+    const [showLogin, setShowLogin] = useState(true);
+    const [restaurant, setRestaurant] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+    const [restaurantSampler, setRestaurantSampler] = useState([]);
+    const navigate = useNavigate();
+
+    return (
+        <div className={styles.authButton}>
+            {showLogin ? (
+                <>
+                    <button onClick={() => setShowLogin(true)}>Login</button>
+                    <Link to="/signup">Sign Up</Link>
+                </>
+            ) : (
+                <>
+                    <button onClick={() => setShowLogin(false)}>Sign Up</button>
+                    <Link to="/login">Login</Link>
+                </>
+            )}
+            { !showLogin ? <SignUpForm signUp={props.signUp}/> : <LoginForm login={props.login}/>}
+        </div>
+    )
 }
