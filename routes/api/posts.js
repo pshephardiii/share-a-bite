a83108ae986be172935ff79f9a8d8370b9024116
Nicole Beechler <nmbeechler@gmail.com>
2024-03-16 16:24:55 -0400

    Added routes for like/unlike posts and restaurants

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,10 +8,22 @@ router.get('/', postCtrl.index, postCtrl.jsonPosts)
 // Delete DELETE /posts/:id
 router.delete('/:id', userCtrl.auth, postCtrl.destroy, postCtrl.jsonPost)
 // Update PUT /posts/:id
-router.put('/:id', userCtrl.auth, blogCtrl.update, postCtrl.jsonPost)
+router.put('/:id', userCtrl.auth, postCtrl.update, postCtrl.jsonPost)
 // Create GET /posts
 router.post('/', userCtrl.auth, postCtrl.create, postCtrl.jsonPost)
 // Show GET /posts/:id
 router.get('/:id', postCtrl.show, postCtrl.jsonPost)
+
+// Liked by
+router.post('/:id/likes', userCtrl.auth, postCtrl.likePost, postCtrl.jsonPost)
+
+// UN-liked
+router.delete('/:id/likes', userCtrl.auth, postCtrl.unlikePost, postCtrl.jsonPost)
+
+// Favorited restaurants
+router.get('/:id/restaurants', userCtrl.auth, postCtrl.getFeaturedRestaurant, postCtrl.jsonPost)
+
+// UN-Favorited restaurants
+router.delete('/:id/restaurants', userCtrl.auth, postCtrl.unFavoriteRestuarant, postCtrl.jsonPost)
 
 module.exports = router
