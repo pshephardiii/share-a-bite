82b827d10e7b42bf3b34cef7cec9ad267573ecbc
Paul Shephard <paulshephard598@gmail.com>
2024-03-19 19:51:59 -0400

    tweaks to the user backend after testing

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -57,8 +57,19 @@ exports.updateUser = async (req, res) => {
 
 exports.userIndex = async (_, res ,next) => {
     try {
-        const users = await User.find({ user: req.body.user })
-        res.locals.data.users = users
+        const users = await User.find({})
+        res.status(200).json(users)
+
+        next()
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+exports.contactsIndex = async (req, res ,next) => {
+    try {
+        const users = await User.find({ contacts: [req.user._id] })
+        res.status(200).json(users)
         next()
     } catch (error) {
         res.status(400).json({ message: error.message })
@@ -76,11 +87,12 @@ exports.deleteUser = async (req, res) => {
 
     exports.addUserContact = async (req,res) => {
         try {
-            const newUser = await User.findById(res.params.id)
+            const newUser = await User.findById(req.params.id)
             req.user.contacts.addToSet(newUser._id)
-            newUser.contacts.addToSet(req.body._id)
+            newUser.contacts.addToSet(req.user._id)
             await req.user.save()
             await newUser.save()
+            res.status(200).json({ newUser })
         }catch(error){
             res.status(400).json({ message: error.message })
         }
@@ -88,12 +100,13 @@ exports.deleteUser = async (req, res) => {
 
     exports.deleteUserContact = async (req,res) => {
         try {
-            const newUser = await User.findById(res.params.id)
+            const newUser = await User.findById(req.params.id)
             const index = req.user.contacts.indexOf(newUser._id )
             const index2 = newUser.contacts.indexOf(req.user._id)
             req.user.contacts.splice(index, 1)
             newUser.contacts.splice(index2, 1)
             await req.user.save()
+            await newUser.save()
             res.status(200).json({ message: 'Contact Deleted' })
         }catch(error){
             res.status(400).json({ message: error.message })
