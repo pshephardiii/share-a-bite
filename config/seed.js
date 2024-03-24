33e6af7e2606672bd1a5bb2962a4f93375b1ef3b
Paul Shephard <paulshephard598@gmail.com>
2024-03-24 19:32:54 -0400

    jazzed up the seed file a bit

--- config/seed.js
+++ config/seed.js
@@ -34,11 +34,11 @@ function log() {
 
     await Post.deleteMany({})
     const posts = await Post.create([
-        {title: "Good food!", dish: "chicken", user: users[0]},
-        {title: "Amazing Dish!", dish: "beef", user: users[1]},
-        {title: "Outstanding!", dish: "fish", user: users[2]},
-        {title: "Yum!", dish: "pasta", user: users[3]},
-        {title: "FOOD!", dish: "lobster", user: users[4]}
+        {title: "Good food!", body: "wow what fantastic chicken! I can't believe it!", rating: 5, dish: "chicken", restaurant: restaurants[2], user: users[0]},
+        {title: "Pretty good!", body: "it was good, but the presentation leaves a lot to be desired", rating: 3, dish: "beef", restaurant: restaurants[0], user: users[1]},
+        {title: "Outstanding!", body: "I had an out of body experience with this fish... it was a little scary", rating: 4, dish: "fish", restaurant: restaurants[1], user: users[2]},
+        {title: "Ehhh", body: "Was this pasta even cooked?", rating: 2, dish: "pasta", restaurant: restaurants[4], user: users[3]},
+        {title: "FOOD!", body: "I ordered a burger", rating: 1, dish: "lobster", restaurant: restaurants[3], user: users[4]}
     ])
 
     await User.findOneAndUpdate({name: "Paul"}, {posts: posts[0]}, {new: true})
