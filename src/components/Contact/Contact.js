a00a8a7686604a077e6d8036036e1a0d70123985
junfengqiao <jfqemma@gmail.com>
2024-03-26 19:31:28 -0400

    removed the sidebar and show the contactList

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,19 @@
 import styles from "./Contact.module.scss"
+import { useNavigate } from "react-router-dom"
+
 
 export default function Contact({
     contact, deleteContact}){
+    const navigate = useNavigate()
     return(
         <div className={styles.contact}> 
+
+            <h3 onClick={()=>navigate(`/usershowpage/${contact._id}`)}>{contact.name}</h3>
+            <div onClick={()=>navigate(`/usershowpage/${contact._id}`)}><img className={styles.profilePic} src={contact.pic} /></div>
+            <div className={styles.button}> 
+            <button onClick={()=>{deleteContact(contact._id)}}>unfollow</button>
+            </div>
         
-        <h3>{contact.name}</h3>
-        <div className={styles.button}> 
-        <button onClick={()=>{deleteContact(contact._id)}}>unfollow</button>
-           </div>
         </div>
     )
 }
